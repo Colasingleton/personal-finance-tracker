e264ba614b7d54a4ec62fb744751fa198a27aec8
cola <csin120@wgu.edu>
2025-05-28 16:01:37 -0400

    login screen and dashboard created / added a test user

--- src/main/java/com/financetracker/expense_tracker/controller/AuthController.java
+++ src/main/java/com/financetracker/expense_tracker/controller/AuthController.java
@@ -1,4 +1,4 @@
-package controller;
+package com.financetracker.expense_tracker.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
